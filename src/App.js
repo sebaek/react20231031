2b412f03342710c3b8faf2a78f1eb667804ff0f3
sebaek <sebaek@gmail.com>
2023-10-31 14:24:46 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://i.imgur.com/SuL4x4v.jpeg"
+        alt="경복궁"
+      />
+      ;
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <MyComponent1>
-      <MyComopnent1>
-      <MyComponent1>
-    {/*
+      <MyComponent1></MyComponent1>
+      <MyComponent1 />
+      <MyComponent1 />
+      {/*
       <img src="https://i.imgur.com/SuL4x4v.jpeg" />
       <img src="https://i.imgur.com/SuL4x4v.jpeg" />
       <img src="https://i.imgur.com/SuL4x4v.jpeg" />
