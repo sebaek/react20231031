af5e8931262cf400e98c6bd0e2122f88fafe336f
sebaek <sebaek@gmail.com>
2023-10-31 11:49:26 +0900

    component 연습

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+      <MyComponent2 />
+      {/*
+        <p>lorem ipsum alkdjf alkdjf </p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+       */}
     </div>
   );
 }
+
+
 
 export default App;
