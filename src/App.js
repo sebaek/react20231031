8911a275bfe24e4af8f1a91a77d52d27227b3054
sebaek <sebaek@gmail.com>
2023-10-31 12:14:33 +0900

    component 연습, 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+
+function MyComponent2() {
+    return (
+        <>
+            {/*p>lorem5^ul>li*3>lorem1*/}
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Labore.</li>
+                <li>Quas?</li>
+            </ul>
+        </>
+    );
+}
 
 function App() {
   return (
