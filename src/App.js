2b39386623e4bd9c710c205d1119d95d3a265e38
sebaek <sebaek@gmail.com>
2023-10-31 15:26:04 +0900

    jsx의 {}에서 간단한 연산자 활용 연습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 function App() {
-  return (
-    <div>
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
 
-    </div>
-  );
+  return <div></div>;
+
+  // 두 수를 출력하고
+  // 두 수가 같으면
+  // "당첨!!"
+  // 두 수가 다르면
+  // "다시 던져 보세요"
 }
 
 export default App;
