e86dcc9c4c80cd04e27a6b2c09d9e9096d4ced06
sebaek <sebaek@gmail.com>
2023-10-31 14:49:04 +0900

    jsx에서 js 코드 작성은 {} 사용 연습 해보기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "01099999999";
+  const age = 44;
+
+  return <></>;
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp />
+      {/*
+       <h1>age는 44</h1>
+       <h1>address는 seoul</h1>
+       <h1>country는 korea</h1>
+       <h1>전화번호는 01099999999</h1>
+     */}
     </div>
   );
 }
