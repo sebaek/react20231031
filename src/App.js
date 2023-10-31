91c0f222b05a0d99b050b01679e4f36e49fc4e19
sebaek <sebaek@gmail.com>
2023-10-31 15:54:14 +0900

    프로퍼티의 값은 자바스크립트 코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 function App() {
-  const name = "흥민";
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+
+  let messageClassName = "lose";
+  let message = "다시 던져 보세요";
+
+  if (num1 === num2) {
+    messageClassName = "win";
+    message = "당첨";
+  }
+
   return (
     <>
-      <h1>이름은 {name}</h1>
-      <h1>이름은 {"강인"}</h1>
-      <h1>이름은 희찬</h1>
+      <h1>1번 주사위 {num1}</h1>
+      <h1>2번 주사위 {num2}</h1>
 
-      <div className={"error"}>Lorem ipsum dolor.</div>
-      <div className="error">Lorem ipsum dolor.</div>
+      <h1 className={messageClassName}>{message}</h1>
     </>
   );
 }
