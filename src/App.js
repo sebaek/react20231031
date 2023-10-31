2ca40fa996c15af8915bad1cf370f0e1b96c9950
sebaek <sebaek@gmail.com>
2023-10-31 15:42:22 +0900

    jsx에서 js 키워드 사용 불가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 function App() {
-  // 1~100
-  const number = Math.ceil(Math.random() * 100);
-  const isLarge = number > 50;
-  const isSmall = number <= 50;
-
+  // jsx가 js 코드로 변환됨
+  // 따라서 js 키워드를 사용할 수 없음
   return (
     <>
-      <h1>난수 : {number}</h1>
-      <h1>{number > 50 ? "큰 수" : "작은 수"}</h1>
-      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
-      <div>{isLarge && <h1>큰 수다!!</h1>}</div>
-      <div>{isLarge || <h1>작은 수다!!</h1>}</div>
+      {/* class 속성은 className으로 작성 */}
+      <div className="header">Lorem ipsum dolor.</div>
+      <div className="error">Lorem ipsum dolor.</div>
+      {/* for 속성은 htmlFor로 작성 */}
+      <div>
+        <label htmlFor="nameInput">이름</label>
+        <input type="text" id="nameInput" />
+      </div>
+      <div>
+        <label htmlFor="ageInput"></label>
+        <input type="text" id="ageInput" />
+      </div>
     </>
   );
-}
-
-function BigImage() {
-  return <h1>큰 이미지</h1>;
-}
-
-function SmallImage() {
-  return <h3>작은 이미지</h3>;
 }
 
 export default App;
