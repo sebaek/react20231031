c011be875ec9b067c6ddfa3ac135b394fdd43aa7
sebaek <sebaek@gmail.com>
2023-11-01 14:19:25 +0900

    default export, import 연습시작

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import MyBox, { person, team } from "./component/MyContainer";
 
 function App() {
   return (
     <div>
-
+      <MyBox />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
     </div>
   );
 }
