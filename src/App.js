156b3a19e694de7f0957534287ba9f391765f9eb
sebaek <sebaek@gmail.com>
2023-11-01 10:46:00 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+function MyElem({ name, age, team }) {
+  return (
+    <div>
+      <p>
+        {name}선수는 {age}살이고 {team}에서 뜁니다.
+      </p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
