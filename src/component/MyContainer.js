de7484bc09a9f655f79a5aee3ee654f9c427afb6
sebaek <sebaek@gmail.com>
2023-11-01 14:26:58 +0900

    default export, import 연습 완료

---
+++ src/component/MyContainer.js
@@ -0,0 +1,12 @@
+function MyContainer() {
+  return (
+    <div>
+      <h1>THIS IS CONTAINER</h1>
+    </div>
+  );
+}
+
+export const person = { name: "son", address: "seoul" };
+export const team = { title: "paris", score: 8.9 };
+
+export default MyContainer;
