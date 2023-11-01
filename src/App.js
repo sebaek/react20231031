3537584927a3db2857bbc73ac4657770639e5f89
sebaek <sebaek@gmail.com>
2023-11-01 11:13:27 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-  return <div>{/*  MyBox 컴포넌트 2번 사용해 보기 */}</div>;
+  return (
+    <div>
+      <MyBox
+        p="10px"
+        m="20px"
+        bg="gold"
+        color="grey"
+        text="my name is component"
+      />
+      <MyBox
+        p="7px"
+        m="15px"
+        bg="skyblue"
+        color="red"
+        text="오늘 점심 머먹지?"
+      />
+    </div>
+  );
 }
 
 function MyBox({ p, m, bg, color, text }) {
