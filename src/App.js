9a87785a719849ed915cac58954a1907f2de06ed
sebaek <sebaek@gmail.com>
2023-11-01 11:22:11 +0900

    props 구조분해할당, 기본값 주기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 function App() {
   return (
     <>
-      <MyComp value="흥민" address="신촌" />
-      <MyComp value="강인" address="강남" />
+      <MyComp name="흥민" address="신촌" age={33} city="서울" />
+      <MyComp address="강남" age={22} city="부산" />
+      <MyComp address="런던" city="영국" />
+      <MyComp name="희찬" age={66} city="파리" address="프랑스" />
     </>
   );
 }
 
-// {value: "흥민", address: "신촌"}
-function MyComp({ value, address }) {
+function MyComp({ name = "anonymous", address, city, age = 100 }) {
   return (
     <div>
-      <h1>
-        {value}는 {address}에 산다
-      </h1>
+      <p>
+        {name} 은 {age}세 이고 {address}, {city}에 산다
+      </p>
     </div>
   );
 }
