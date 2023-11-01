e67443b15da0675e0c8b2c85442c806e1a866bf9
sebaek <sebaek@gmail.com>
2023-11-01 16:20:47 +0900

    보통의 css 파일을 사용할 때 클래스명 중복이 발생할 수 있음 -> css module로 중복 방지

--- src/component/MyBlock.js
+++ src/component/MyBlock.js
@@ -1,9 +1,9 @@
-import "../style/yourStyle.css";
+import css from "../style/yourStyle.module.css";
 
 export function MyBlock() {
   return (
     <div>
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      <h1 className={css.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,11 +1,11 @@
-import "../style/mystyle.css";
+import css from "../style/mystyle.module.css";
 export function MyText() {
   return (
     <div>
       {/* red 배경색, white 글자색  */}
-      {/* src/style/mystyle.css 파일 생성 */}
-      {/* MyText.js 에서 mystyle.css import 하기 */}
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      {/* src/style/mystyle.module.css 파일 생성 */}
+      {/* MyText.js 에서 mystyle.module.css import 하기 */}
+      <h1 className={css.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
