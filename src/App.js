c1e046898bef50b93538f224fc4ff2a44baf1d67
sebaek <sebaek@gmail.com>
2023-11-01 10:31:35 +0900

    props destructuring assignment 구조분해할당

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 function App() {
   return (
     <>
-      <h1
-        className="note"
-        style={{ backgroundColor: "blue", color: "white" }}
-        title="제목 요소"
-      >
-        Lorem.
-      </h1>
-      <p title="문단 요소">Lorem ipsum dolor.</p>
-      <MyComp title="내 컴포넌트" name="흥민" />
-      <MyComp title="I am 컴포넌트에요" name="강인" />
+      <MyComp value="흥민" address="신촌" />
+      <MyComp value="강인" address="강남" />
     </>
   );
 }
 
-// 함수 인자로
-// 사용한 곳에서 넘긴 property들이 객체형태로 담김
-// 예) title="내 컴포넌트"  => {title: "내 컴포넌트"}
-// 예) title="내 컴포넌트" name="흥민"  => {title: "내 컴포넌트", name: "흥민"}
-function MyComp(props) {
-  console.log("MyComp가 받은 props", props);
+// {value: "흥민", address: "신촌"}
+function MyComp({ value, address }) {
   return (
     <div>
-      <h1 title={props.title}>hello {props.name}</h1>
+      <h1>
+        {value}는 {address}에 산다
+      </h1>
     </div>
   );
 }
