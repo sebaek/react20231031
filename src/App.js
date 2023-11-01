d2aee5ce666afb91d127c5990a0b6cba4dd806b7
sebaek <sebaek@gmail.com>
2023-11-01 16:15:03 +0900

    보통의 css 파일을 사용할 때 클래스명 중복이 발생할 수 있음

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { MyText } from "./component/MyText";
+import { MyBlock } from "./component/MyBlock";
 
 function App() {
   return (
     <div>
       <MyText />
+      <MyBlock />
     </div>
   );
 }
---
+++ src/component/MyBlock.js
@@ -0,0 +1,9 @@
+import "../style/yourStyle.css";
+
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
