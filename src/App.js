99ef618a788b3ef93d42740199f0834c07b79b94
sebaek <sebaek@gmail.com>
2023-11-01 11:25:24 +0900

    props 구조분해할당, 기본값 주기, 연습시작

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
 function App() {
   return (
     <div>
-
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+      <MyComp p="10px" color="gold" text="hello react" />
     </div>
   );
 }
