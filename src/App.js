af08240f7a4ee64e90766d842947872d7e569e0c
sebaek <sebaek@gmail.com>
2023-11-02 15:15:22 +0900

    Select 사용

--- src/App.js
+++ src/App.js
@@ -1,58 +1,20 @@
 import React from "react";
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>띄어쓰기 없이 입력하세요.</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={5}>
-        <Box w="480px">
-          <FormControl>
-            <FormLabel>하나만 선택하세요.</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Lorem.</Radio>
-                <Radio value="value4">Lorem.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box w="480px">
-          <FormControl>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem.</Checkbox>
-                <Checkbox colorScheme="green">Commodi.</Checkbox>
-                <Checkbox colorScheme="purple">Voluptatem.</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"}>
+        <FormControl>
+          <Select placeholder="선택하세요." multiple h={"120px"}>
+            <option value="value1">Lorem.</option>
+            <option value="value2">Voluptates?</option>
+            <option value="value3">Sapiente.</option>
+            <option value="value4">Ex?</option>
+            <option value="value5">Recusandae!</option>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 }
