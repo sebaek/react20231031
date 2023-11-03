155ddf4c740b7cc3a33c81239c86a413f87d3282
sebaek <sebaek@gmail.com>
2023-11-03 14:11:34 +0900

    event.stopPropagation : event bubbling 막는 기능

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
+import { Button, Center } from "@chakra-ui/react";
 
 function App(props) {
   function handleClick(e) {
-    // 브라우저가 해야하는 기본 기능을 취소함
-    e.preventDefault();
-    console.log("다른 일을 시킴");
-  }
-
-  function handleSubmit(e) {
-    // 기본 기능 취소
-    e.preventDefault();
-    console.log("다른일을 시킴");
+    // event bubbling 막는 메소드
+    e.stopPropagation();
+    console.log(e.target.className);
   }
   return (
-    <div>
-      <a href="https://www.naver.com" onClick={handleClick}>
-        네이버
-      </a>
-      <div>
-        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
-          <input type="text" name="q" />
-          <button>검색</button>
-        </form>
-      </div>
-    </div>
+    <Center
+      onClick={handleClick}
+      className="outerBox"
+      w="200px"
+      h="200px"
+      bg="gold"
+    >
+      <Center
+        onClick={handleClick}
+        className="innerBox"
+        w="100px"
+        h="100px"
+        bg="blue"
+      >
+        <Button onClick={handleClick} className="button" colorScheme="yellow">
+          Button
+        </Button>
+      </Center>
+    </Center>
   );
 }
 
