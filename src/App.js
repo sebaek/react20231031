c1f20f9838ca367f6fdc300ce47de4d531f37eee
sebaek <sebaek@gmail.com>
2023-11-03 14:48:44 +0900

    state 가 바뀌면 component가 다시 그려짐(re-render)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { Button, Center } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button, Input } from "@chakra-ui/react";
 
 function App(props) {
-  function handleClick(e) {
-    // event bubbling 막는 메소드
-    e.stopPropagation();
-    console.log(e.target.className);
+  // 상태(state)가 변경(이전 state와 달라짐)되면
+  // Component를 다시 그림 (re render)
+
+  // state : react가 관리하는 component의 값
+
+  // state를 얻는 방법
+  // useState();
+
+  // 배열을 리턴함
+  // index 0 : 현재상태(state)
+  // index 1 : state를 변경할 수 있는 메소드
+  // const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
+
+  // 상태가 바뀔 때에만 컴포넌트가 re render됨
+
+  let text = "초기 메시지";
+  function handleInputChange(e) {
+    text = e.target.value;
+    console.log("text", text);
+  }
+
+  function handleButtonClick(e) {
+    setMessage("바뀐 상태");
   }
   return (
-    <Center
-      onClick={handleClick}
-      className="outerBox"
-      w="200px"
-      h="200px"
-      bg="gold"
-    >
-      <Center
-        onClick={handleClick}
-        className="innerBox"
-        w="100px"
-        h="100px"
-        bg="blue"
-      >
-        <Button onClick={handleClick} className="button" colorScheme="yellow">
-          Button
-        </Button>
-      </Center>
-    </Center>
+    <div>
+      <Input onChange={handleInputChange} />
+      <p>{text}</p>
+      <Button onClick={handleButtonClick}>상태 바꾸기</Button>
+      <Button onClick={() => setMessage("")}>빈 문자열로 바꾸기</Button>
+      <p>{message}</p>
+    </div>
   );
 }
 
