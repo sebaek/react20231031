7d0d9ef70f7831ea5a9928642c1bc9c9d93ae012
sebaek <sebaek@gmail.com>
2023-11-03 11:49:30 +0900

    변수의 값이 변경되어도 바로 dom에 반영되지 않음

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Input } from "@chakra-ui/react";
 
 function App(props) {
+  let text = "initial text";
   function handleChange(e) {
     // console.log("인풋값이 변경됨");
     console.log(e.target.value);
+    text = e.target.value;
+    console.log("text", text);
   }
 
   return (
     <div>
+      <p>{text}</p>
       <Input onChange={handleChange} />
       <Input onChange={handleChange} />
     </div>
