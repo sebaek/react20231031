744b31f0f85be775e21db2e1a9b9c0ef7794400f
sebaek <sebaek@gmail.com>
2023-11-03 10:31:03 +0900

    함수를 props으로 전달, 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton({ executeClick, children }) {
+  return <Button onClick={executeClick}>{children}</Button>;
+}
+
+// 두개의 버튼 만들기
+// 첫번째 버튼은 클릭하면 콘솔에 "hello"
+// 두번째 버튼은 클릭하면 콘솔에 "greeting"
+
 function App() {
-  return <></>;
+  function sayHello() {
+    console.log("hello");
+  }
+
+  function sayGreeting() {
+    console.log("greeting");
+  }
+
+  return (
+    <>
+      <MyButton executeClick={sayHello}>헬로</MyButton>
+      <MyButton executeClick={sayGreeting}>그리팅</MyButton>
+    </>
+  );
 }
 
 export default App;
