35167c48c9e079423b98129a71f7f79d08ffdeb7
sebaek <sebaek@gmail.com>
2023-11-03 09:23:58 +0900

    children prop 연습시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to="https://www.naver.com">naver 로</MyLink>
+      <MyLink to="https://www.daum.net">daum 으로</MyLink>
+      {/*
+    <a href="https://www.naver.com">naver 로</a>
+    <a href="https://www.daum.net">daum 으로</a>
+
+    */}
+    </>
+  );
 }
 
 export default App;
