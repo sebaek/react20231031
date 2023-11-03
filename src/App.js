066dbf2e8000ba51c8f828448551a061d15797f5
sebaek <sebaek@gmail.com>
2023-11-03 15:34:35 +0900

    state 사용해서 dom 그리기 연습시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  let result = 0;
+  return (
+    <>
+      <input type="number" /> +
+      <input type="number" />= {result}
+    </>
+  );
 }
 
 export default App;
