ef2eda5dc1eea732c225d317d307b1cd1154eecd
sebaek <sebaek@gmail.com>
2023-11-03 12:33:11 +0900

    event.preventDefault() : 기본기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    // a 태그 클릭시 naver로 이동할 수 있게 메소드 작성
+
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+``
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;
