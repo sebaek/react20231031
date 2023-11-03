e8645501b1602de42177c41b9ff0def98b1ade98
sebaek <sebaek@gmail.com>
2023-11-03 16:24:16 +0900

    input 요소에 value, onChange prop 사용

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { useState } from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
+  const [message, setMessage] = useState("");
 
-  function handlePlusButtonClick() {
-    setNumber(number + 1);
-  }
-
-  function handleMinusButtonClick() {
-    setNumber(number - 1);
+  function handleChange(e) {
+    setMessage(e.target.value);
   }
 
   return (
     <div>
-      <input type="number" value={number} />
-      <Box>
-        <Button onClick={handlePlusButtonClick}>+</Button>
-        <Button onClick={handleMinusButtonClick}>-</Button>
-      </Box>
-      <Box>
-        <Button onClick={() => setNumber(number + 1)}>+</Button>
-        <Button onClick={() => setNumber(number - 1)}>-</Button>
-      </Box>
+      <Input type="text" value={message} onChange={handleChange} />
     </div>
   );
 }
