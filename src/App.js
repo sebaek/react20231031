e3f3c4af84cd5582dc8e7015a7a29056677dd2d4
sebaek <sebaek@gmail.com>
2023-11-03 09:33:21 +0900

    children prop 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <>
+      <Button as="a" href={to} colorScheme="orange">
+        {children}
+      </Button>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
