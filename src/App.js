aafabbc471ddd53a91183b3f5c5b2a9eb4d91faa
sebaek <sebaek@gmail.com>
2023-11-03 10:48:22 +0900

    arrow function을 prop으로 전달 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      {/*첫번째 버튼 클릭시 "first" 콘솔에 출력*/}
+      <Button onClick={() => console.log("first")}>Lorem.</Button>
+
+      {/*두번째 버튼 클릭시 "second" 콘솔에 출력*/}
+      <Button
+        onClick={() => {
+          console.log("second");
+        }}
+      >
+        Sunt?
+      </Button>
+    </>
+  );
 }
 
 export default App;
