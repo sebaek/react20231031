6b5a3081342b5f3c926ee8aa7e38c46413d2b36a
sebaek <sebaek@gmail.com>
2023-11-06 16:39:16 +0900

    use immer로 깊은 복사 하는 코드 없이 상태 변경, 연습 시작

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useImmer } from "use-immer";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
+
+let nextId = 1;
+function App(props) {
+  const [items, updateItems] = useImmer([]);
+
+  function handleButtonClick(todo) {
+    updateItems((draft) => {
+      draft.push({ id: nextId++, done: false, text: todo });
+    });
+  }
+
+  function handleDoneButtonClick(id) {
+    console.log(id);
+  }
+
+  return (
+    <div>
+      <Button onClick={() => handleButtonClick("자바공부")}>자바공부</Button>
+      <Button onClick={() => handleButtonClick("점심먹기")}>점심먹기</Button>
+      <Button onClick={() => handleButtonClick("잠자기")}>잠자기</Button>
+
+      <Box>
+        <UnorderedList>
+          {items.map((item) => (
+            <ListItem
+              key={item.id}
+              textDecoration={item.done ? "line-through" : "none"}
+            >
+              {item.text}
+              <Button onClick={() => handleDoneButtonClick(item.id)}>
+                완료
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
