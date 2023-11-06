cf42c65305b1f4132c63c47e81a75b4d4a08ac04
sebaek <sebaek@gmail.com>
2023-11-06 09:34:28 +0900

    state 사용 예제 연습 시작, 완료

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import { useState } from "react";
 function App() {
   const [number, setNumber] = useState(0);
 
+  function handleButtonClick() {
+    setNumber(number + 1);
+  }
+
+  function handleMinusButtonClick() {
+    setNumber(number - 1);
+  }
+
   return (
     <>
       <h1>{number}</h1>
       <Button onClick={handleButtonClick}>증가</Button>
+      <Button onClick={handleMinusButtonClick}>감소</Button>
     </>
   );
 }
