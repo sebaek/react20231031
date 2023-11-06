1c6ca0d09e2b0e62dd421544f8cfe49df3a20d93
sebaek <sebaek@gmail.com>
2023-11-06 16:25:21 +0900

    use immer로 깊은 복사 하는 코드 없이 상태 변경

--- src/App.js
+++ src/App.js
@@ -1,51 +1,38 @@
-import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
+import React from "react";
 import { useImmer } from "use-immer";
+import { Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [person, setPerson] = useState({ name: "son", city: "seoul" });
-  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+  const [person, updatePerson] = useImmer({
+    name: "son",
+    address: { city: "seoul", country: "korea" },
+  });
 
   function handleNameChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.name = e.target.value;
-
-    setPerson(nextPerson);
-  }
-
-  function handleCityChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.city = e.target.value;
-
-    setPerson(nextPerson);
-  }
-
-  function handleUserNameChange(e) {
-    updateUser((draft) => {
+    updatePerson((draft) => {
       draft.name = e.target.value;
     });
   }
 
-  function handleUserCityChange(e) {
-    updateUser((draft) => {
-      draft.city = e.target.value;
+  function handleCityChange(e) {
+    updatePerson((draft) => {
+      draft.address.city = e.target.value;
+    });
+  }
+
+  function handleCountryChange(e) {
+    updatePerson((draft) => {
+      draft.address.country = e.target.value;
     });
   }
 
   return (
     <div>
       <Input value={person.name} onChange={handleNameChange} />
-      <Input value={person.city} onChange={handleCityChange} />
+      <Input value={person.address.city} onChange={handleCityChange} />
+      <Input value={person.address.country} onChange={handleCountryChange} />
       <Text>
-        {person.name}은 {person.city}에 삽니다.
-      </Text>
-
-      <hr />
-
-      <Input value={user.name} onChange={handleUserNameChange} />
-      <Input value={user.city} onChange={handleUserCityChange} />
-      <Text>
-        {user.name}은 {user.city}에 삽니다.
+        {person.name}은 {person.address.country}, {person.address.city} 에 산다
       </Text>
     </div>
   );
