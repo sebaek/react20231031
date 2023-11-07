710ec57ad07de1abe9c5a6d20cc7dbbfaf28e635
sebaek <sebaek@gmail.com>
2023-11-07 16:37:03 +0900

    ajax get 사용 state 업데이트 연습 완료 여러 property 가져와 보기

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState({});
 
   // useEffect로
-  // get 요청 /api/main1/sub2
-  // 받은 값으로 message state 업데이트
+  // get 요청 /api/main1/sub3
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data));
+      .then((data) => setCustomer(data));
   }, []);
 
   return (
     <div>
-      <h1>{message}</h1>
+      <h1>{customer.id}</h1>
+      <h1>{customer.name}</h1>
+      <h1>{customer.contactName}</h1>
+      <h1>{customer.address}</h1>
+      <h1>{customer.city}</h1>
+      <h1>{customer.postalCode}</h1>
+      <h1>{customer.country}</h1>
     </div>
   );
 }
