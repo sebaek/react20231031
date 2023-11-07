3157b734aa0a3fedc6b606a39104c9f321c7c26b
sebaek <sebaek@gmail.com>
2023-11-07 15:48:32 +0900

    ajax get 사용 state 업데이트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [myState, setMyState] = useState("");
+
   useEffect(() => {
     axios
       .get("/api/main1/sub1")
       .then((response) => response.data)
-      .then((data) => console.log(data))
+      .then((data) => setMyState(data))
       .catch((error) => console.log(error));
   }, []);
-  return <div></div>;
+
+  return <div>{myState}</div>;
 }
 
 export default App;
