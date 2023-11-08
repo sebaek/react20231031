de1ef7c8500870a6f0da3bfbc8b97afd4628adb2
sebaek <sebaek@gmail.com>
2023-11-08 16:30:40 +0900

    react router의 useSearchParams로 query string 읽기

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Link,
   Outlet,
   Route,
   RouterProvider,
   useNavigate,
+  useSearchParams,
 } from "react-router-dom";
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
+import axios from "axios";
 
-function HomeComponent() {
-  // 경로 이동시 useNavigate hook 사용 해야함
+function Home() {
   const navigate = useNavigate();
 
   return (
     <Box>
-      <Flex gap={"10px"}>
-        {/* 경로 이동시 javascript 코드를 그냥 쓰면 안됨 */}
-        <Box>
-          <Button onClick={() => (window.location.href = "/apath")}>
-            a로 가기
-          </Button>
-        </Box>
-        <Box>
-          <Button onClick={() => (window.location.href = "/bpath")}>
-            b로 가기
-          </Button>
-        </Box>
-        <Box>
-          <Button onClick={() => navigate("/apath")}>a로 가기</Button>
-        </Box>
-        <Box>
-          <Button onClick={() => navigate("/bpath")}>b로 가기</Button>
-        </Box>
-      </Flex>
-      <Outlet />
+      <Box>
+        <Button onClick={() => navigate("/path1?id=1")}>1번 고객 보기</Button>
+        <Button onClick={() => navigate("/path1?id=2")}>2번 고객 보기</Button>
+        <Button onClick={() => navigate("/path1?id=3")}>3번 고객 보기</Button>
+      </Box>
+      <Box>
+        <Outlet />
+      </Box>
     </Box>
   );
 }
 
 function AComp() {
-  return <Box>에이 컴포넌트</Box>;
-}
+  const [customer, setCustomer] = useState(null);
+  // query string 을 얻기
+  const [searchParams] = useSearchParams();
 
-function BComp() {
-  return <Box>비 컴포넌트</Box>;
+  // console.log(searchParams);
+  // console.log(searchParams.get("id"));
+  // console.log(searchParams.toString());
+
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub4?" + searchParams.toString())
+      .then((response) => setCustomer(response.data));
+  }, [searchParams]);
+
+  return (
+    <Box>
+      {customer && (
+        <Text>
+          {searchParams.get("id")} 번 고객명 {customer.name}
+        </Text>
+      )}
+    </Box>
+  );
 }
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<HomeComponent />}>
-      <Route path="apath" element={<AComp />} />
-      <Route path="bpath" element={<BComp />} />
+    <Route path="/" element={<Home />}>
+      <Route path="path1" element={<AComp />} />
     </Route>,
   ),
 );
