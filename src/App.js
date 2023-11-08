6a0037c1a92683cc8693a541b9088bd63479e8d4
sebaek <sebaek@gmail.com>
2023-11-08 15:31:51 +0900

    react router의 Link를 사용할 것

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Link,
   Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function HomeComponent() {
   return (
     <Box>
-      공통된 요소들
+      <Flex gap={"10px"}>
+        {/* react-router 사용시 a 태그를 사용하지 않을 것 */}
+        <Box>
+          <a href="/apath">에이로 가기</a>
+        </Box>
+        <Box>
+          <a href="/bpath">비로 가기</a>
+        </Box>
+
+        {/* 대신 Link Component 사용 */}
+        <Box>
+          <Link to={"/apath"}>에이로 가기</Link>
+        </Box>
+        <Box>
+          <Link to={"/bpath"}>비로 가기</Link>
+        </Box>
+      </Flex>
       <Outlet />
     </Box>
   );
