97b0657406ab66a27d0c7c419178a7e9069e7487
sebaek <sebaek@gmail.com>
2023-11-08 15:26:20 +0900

    상위 컴포넌트와 Outlet

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 
+function HomeComponent() {
+  return (
+    <Box>
+      공통된 요소들
+      <Outlet />
+    </Box>
+  );
+}
+
+function AComp() {
+  return <Box>에이 컴포넌트</Box>;
+}
+
+function BComp() {
+  return <Box>비 컴포넌트</Box>;
+}
+
 const routes = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<Box>home page</Box>} />
-      <Route path="/path1" element={<Box>경로1</Box>} />
-      <Route path="/path2" element={<Box>경로2</Box>} />
-      <Route path="/path3" element={<Box>경로3</Box>} />
-      <Route path="/main1/path1" element={<Box>경로4</Box>} />
-      <Route path="/main1/path2" element={<Box>경로5</Box>} />
-      <Route path="/main2">
-        <Route path="path1" element={<Box>경로6</Box>} />
-        <Route path="path2" element={<Box>경로7</Box>} />
-        <Route path="path3">
-          <Route path="sub1" element={<Box>경로8</Box>} />
-          <Route path="sub2" element={<Box>경로9</Box>} />
-        </Route>
-      </Route>
-    </>,
+    <Route path="/" element={<HomeComponent />}>
+      <Route path="apath" element={<AComp />} />
+      <Route path="bpath" element={<BComp />} />
+    </Route>,
   ),
 );
 
