defbe9dd0112f7c7fa119150fb23e4a1bc21e76c
sebaek <sebaek@gmail.com>
2023-11-08 10:30:17 +0900

    useEffect 연습 시작

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(props) {
+  return (
+    // 직원 번호를 선택하면 직원의 이름이 출력
+    // /api/main1/sub5?id=5
+    // spring boot의 메소드도 작성하기
+    <div></div>
+  );
+}
+
+export default App;
