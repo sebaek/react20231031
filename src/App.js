6ecd262365da9963aa5658292288abefa1e16d64
sebaek <sebaek@gmail.com>
2023-11-08 10:24:16 +0900

    useEffect 예제

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Box, Select, Text } from "@chakra-ui/react";
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import axios from "axios";
 
 function App(props) {
   const [customerId, setCustomerId] = useState(0);
   const [customer, setCustomer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("/api/main1/sub4?id=" + customerId)
       .then((response) => response.data)
       .then((data) => setCustomer(data))
-      .catch((error) => console.log(error))
-      .finally(() => console.log("끝!!"));
+      .catch((error) => setCustomer(null))
+      .finally(() => setIsLoading(false));
   }, [customerId]);
 
   return (
@@ -34,7 +36,17 @@ function App(props) {
         <option value="10">10</option>
       </Select>
       <Box>
-        <Text>고객 이름 : {customer.name}</Text>
+        {isLoading && <Spinner />}
+
+        {isLoading || (
+          <>
+            {customer === null ? (
+              <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
+            ) : (
+              <Text>고객 이름 : {customer.name}</Text>
+            )}
+          </>
+        )}
       </Box>
     </div>
   );
